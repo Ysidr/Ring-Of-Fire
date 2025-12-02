cdf17032577382f01d4a81e7e230a7cc96d10c7e
Ysidro <39842830+Ysidr@users.noreply.github.com>
2025-12-02 14:02:02 +0100

    full stack added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -10,10 +10,24 @@ import { CommonModule } from '@angular/common';
 })
 export class GameComponent {
   isTaken = false;
+  game!: Game;
+  currentCard: string = "";
+
+  ngOnInit(): void {
+    this.newGame();
+    console.log(this.game);
+  }
 
 
     takeCard() {
         this.isTaken = true;
+        this.currentCard = this.game.Stack.pop() || "";
+        this.game.LayedCards.push(this.currentCard);
+        console.log(this.currentCard);
+    }
+
+    newGame() {
+        this.game = new Game()  ;
     }
 
 }
---
+++ src/models/game.ts
@@ -0,0 +1,46 @@
+export class Game {
+    public Players: string[] = [];
+    public Stack: string[] = [];
+    public LayedCards: string[] = [];
+    public CurrentPlayer: number = 0;
+
+    constructor() {
+        for (let i = 2; i < 11; i++) {
+            this.Stack.push(i + "_of_hearts");
+            this.Stack.push(i + "_of_diamonds");
+            this.Stack.push(i + "_of_clubs");
+            this.Stack.push(i + "_of_spades");
+
+        } 
+        addSpecialCards(this);
+        shuffleArray(this.Stack);
+    }
+}
+
+function addSpecialCards(game: Game) {
+    game.Stack.push("ace_of_hearts");
+    game.Stack.push("ace_of_diamonds");
+    game.Stack.push("ace_of_clubs");
+    game.Stack.push("ace_of_spades");
+    game.Stack.push("jack_of_hearts2");
+    game.Stack.push("jack_of_diamonds2");
+    game.Stack.push("jack_of_clubs2");
+    game.Stack.push("jack_of_spades2");
+    game.Stack.push("queen_of_hearts2");
+    game.Stack.push("queen_of_diamonds2");
+    game.Stack.push("queen_of_clubs2");
+    game.Stack.push("queen_of_spades2");
+    game.Stack.push("king_of_hearts2");
+    game.Stack.push("king_of_diamonds2");
+    game.Stack.push("king_of_clubs2");
+    game.Stack.push("king_of_spades2");
+    game.Stack.push("red_joker");
+    game.Stack.push("black_joker");
+}
+
+function shuffleArray(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
