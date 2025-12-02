d48608955931ce7c30bcb9f48bca5048feece4a6
Ysidro <39842830+Ysidr@users.noreply.github.com>
2025-12-02 16:06:19 +0100

    players can be added

---
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-dialog-add-player',
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDialogModule],
+  templateUrl: './dialog-add-player.component.html',
+  styleUrl: './dialog-add-player.component.scss'
+})
+export class DialogAddPlayerComponent {
+  playerName: string = '';
+
+  onNoClick(): void {
+    // Handle no click if needed
+  }
+}
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,18 +2,28 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 
 @Component({
   selector: 'app-game',
-  imports: [CommonModule, PlayerComponent],
+  standalone: true,
+  imports: [CommonModule, PlayerComponent, MatIconModule, MatButtonModule, MatDialogModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
+
+
 export class GameComponent {
   isTaken = false;
   game!: Game;
   currentCard: string = "";
+
+  constructor(public dialog: MatDialog) {
+  }
 
   ngOnInit(): void {
     this.newGame();
@@ -40,6 +50,17 @@ export class GameComponent {
 
   newGame() {
     this.game = new Game();
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent, {
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.game.Players.push(result);
+      }
+    });
   }
 
 }
