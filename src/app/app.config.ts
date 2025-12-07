1105f7ded8bc89c451e3f208307d7ff93d9f3b3d
Ysidro <39842830+Ysidr@users.noreply.github.com>
2025-12-07 16:26:07 +0100

    Firebase init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "ring-of-fire-c5a83", appId: "1:597488621602:web:c821aaf286c51abc78527b", storageBucket: "ring-of-fire-c5a83.firebasestorage.app", apiKey: "AIzaSyDUdnN8wWDNpj-oPl3hmj3ZDl0ORAxXOgc", authDomain: "ring-of-fire-c5a83.firebaseapp.com", messagingSenderId: "597488621602", projectNumber: "597488621602", version: "2" })), provideFirestore(() => getFirestore())]
 };
